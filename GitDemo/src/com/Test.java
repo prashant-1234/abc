e0532378ea995c63d0b814195511d0b31498cd34
admin <admin@Lenovo-PC>
2018-07-28 13:32:12 +0530

    jajajajjja

--- GitDemo/src/com/Test.java
+++ GitDemo/src/com/Test.java
@@ -6,7 +6,7 @@ public class Test
    {
 	System.out.println("prasant maruti");
 	System.out.println(" hi prashant ==================================================we come to capita ");
-	System.out.println("hiiiiii    hello    ");
+	System.out.println("hiiiiii    hello  xxxxxxxxxxxxxxxxxxxxxxxxx  ");
 	
 }
 }
